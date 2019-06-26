38a705ce39a853388801a9e168b3f9002ab9cb29
Alan Carando <alan.m.carando@gmail.com>
2019-06-25 22:35:03 -0300

    Final video 20

---
+++ Ejercicio 05 - http request/js/app.js
@@ -0,0 +1,23 @@
+(function() {
+
+    var app = angular.module('ejemplosApp', []);
+
+
+
+    app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+
+        $scope.profesores = {};
+
+        $http.get('json/profesores.json').success(function(data) {
+            //Codigo cuando es correcta la peticion
+            $scope.profesores = data.profesores;
+        });
+
+
+    }]);
+
+
+
+
+
+})();
